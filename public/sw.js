6495955505184ce859c0085ce6ddcf50cb107271
DavidDav59 <vertraydavid@gmail.com>
2025-04-16 17:02:13 +0200

    Initial commit

---
+++ public/sw.js
@@ -0,0 +1,10 @@
+self.addEventListener("install", e => {
+  console.log("Service Worker installé");
+  self.skipWaiting();
+});
+self.addEventListener("activate", e => {
+  console.log("Service Worker activé");
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(fetch(e.request).catch(() => new Response("Offline")));
+});
